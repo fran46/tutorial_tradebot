77067d639ddf9949e09ac608d58cee63be7bcfee
Fran <vr46.csgo@gmail.com>
2017-03-21 14:09:54 +0100

    Update userInfo.php

--- steamauth/userInfo.php
+++ steamauth/userInfo.php
@@ -1,6 +1,7 @@
 <?php
 if (empty($_SESSION['steam_uptodate']) or empty($_SESSION['steam_personaname'])) {
 	require 'configuracion.php';
+	require '../conexion.bd.php';
 	$url = file_get_contents("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=".$steamauth['apikey']."&steamids=".$_SESSION['steamid']); 
 	$content = json_decode($url, true);
 	$_SESSION['steam_steamid'] = $content['response']['players'][0]['steamid'];
